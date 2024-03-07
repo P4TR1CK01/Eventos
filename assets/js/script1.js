adcbc31dea8359ea2ec0eb67b45bdca9f7504d5a
Patrick <patrick.ojeda.91@gmail.com>
2024-03-07 02:58:20 -0300

    revisar el correcto funcionamiento de los script

---
+++ assets/js/script1.js
@@ -0,0 +1,32 @@
+// vamos a añadir dinámicamente una etiqueta H2 al container
+// Acá definimos nuestra función
+function add_dinamicamente () {
+  $('.container').append(
+    '<h2 class="subtitulo">Esta etiqueta fué añadida dinámicamente</h2>'
+  )
+  $('.container').append(
+    '<h3 class="titulito">A mi también me añadieron</h3>'
+  )
+  $('.subtitulo').css('color', '#CCC')
+  $('.titulito').css('text-decoration', 'underline')
+}
+
+
+// creamos una función para cambiar de color el H1
+function  cambiar_h1() {
+  $('h1').toggleClass('text-danger')
+}
+// ahora unimos el evento "dblclick" a la función cambiar_h1
+$('h1').on('dblclick', cambiar_h1)
+
+// Forma compacta
+/*
+$('h1').on('dblclick', function  () {
+  $('h1').toggleClass('text-danger')
+})
+*/
+
+// Iniciamos el caorusel de Bootstrap
+const carousel = new bootstrap.Carousel('.carousel')
+
+
